937d829ebefb07ac65eaac6b5835190425591592
nhussain920 <47760206+nhussain920@users.noreply.github.com>
2019-06-09 21:25:17 +0100

    Add files via upload

---
+++ AAEngine.java
@@ -0,0 +1,19 @@
+
+
+import org.adapter.AA1;
+import org.adapter.AA2;
+import org.adapter.AA3;
+
+
+public class AAEngine {
+	
+	public static void main(String[] args) {
+
+		AA1 aa1 = new AA1();
+		AA2 aa2 = new AA2();
+		AA3 aa3 = new AA3();
+		
+		
+	}
+	
+}
